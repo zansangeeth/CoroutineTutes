110d4463f12111a49e8d809d955df461c1885d6b
zansangeeth <zansangeeth17@gmail.com>
2022-09-24 14:05:01 +0530

    Main_IO_Dispatchers

--- app/src/main/java/com/zasa/coroutinesandroid/MainActivity.kt
+++ app/src/main/java/com/zasa/coroutinesandroid/MainActivity.kt
@@ -2,10 +2,10 @@ package com.zasa.coroutinesandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 
 private const val TAG = "MainActivity"
 
@@ -14,19 +14,46 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.i(TAG, "before runBlocking ${Thread.currentThread().name}")
-        runBlocking {
-            Log.i(TAG, "start runBlocking ${Thread.currentThread().name}")
-            launch {
-
-                delay(5000)
-
+        btnButton.setOnClickListener {
+            CoroutineScope(IO).launch{
+                fakeApiRequest()
             }
-            Log.i(TAG, "end runBlocking ${Thread.currentThread().name}")
         }
-        Log.i(TAG, "after runBlocking ${Thread.currentThread().name}")
 
     }
 
+    fun setNewText(input : String){
+        val newText = input
+        tvText.text = newText
+    }
+
+    suspend fun setTextToMainThread(input : String){
+        withContext(Main){
+            setNewText(input)
+        }
+    }
+    suspend fun fakeApiRequest(){
+        var results1 = getApiCall1()
+        setTextToMainThread(results1)
+
+        var results2 = getApiCall2()
+        setTextToMainThread(results2)
+    }
+
+    suspend fun getApiCall1() : String{
+        logThread("getApiCall1")
+        delay(2000)
+        return "results from api-1"
+    }
+
+    suspend fun getApiCall2() : String{
+        logThread("getApiCall2")
+        delay(2000)
+        return "results from api-2"
+    }
+
+    fun logThread(methodName : String){
+        println(" $TAG $methodName : ${Thread.currentThread().name}")
+    }
 
 }
